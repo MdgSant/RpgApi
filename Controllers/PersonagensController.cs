250158d453fbad8247b43b1639a5822cdfd971f6
Victor Picciutti <victorpicciuttidossantos@gmail.com>
2025-09-04 14:10:56 -0300

    RpgApi

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -50,6 +50,29 @@ namespace RpgApi.Controllers
             return Ok(personagens);
         }
 
+        [HttpPut]
+        public IActionResult UpdatePersonagem(Personagem p)
+        {
+            Personagem personagemAlterado = personagens.Find(pers => pers.Id == p.Id);
+            personagemAlterado.Nome = p.Nome;
+            personagemAlterado.PontosVida = p.PontosVida;
+            personagemAlterado.Forca = p.Forca;
+            personagemAlterado.Defesa = p.Defesa;
+            personagemAlterado.Inteligencia = p.Inteligencia;
+            personagemAlterado.Classe = p.Classe;
+
+            return Ok(personagens);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            personagens.RemoveAll(pers => pers.Id == id);
+
+            return Ok(personagens);
+        }
+
+
 
 
 
